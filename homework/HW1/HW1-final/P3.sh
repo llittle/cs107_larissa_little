e3d03e72ec95ef15bc5235b3eed32f80fcc17e38
Larissa <me@larissalittle.com>
2020-09-09 16:00:14 -0400

    Updated to use the find command instead of piping ls and grep

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -1,6 +1,6 @@
 cat apollo13.txt | grep [0-9] | wc -l > apollo_out.txt
 grep --help | grep -w '\-c'
-ls | grep *.py | wc -l
-ls -l -R | grep -G '^-......rw. '|wc -l
-ls -l -R | grep -e '^-......rw. ' -e '^d......rw. ' | wc -l
+find -maxdepth 1 -type f -name "*.py" | wc -l
+find -type f \! -perm -006 |wc -l
+find -type f,d \! -perm -006 | wc -l
 
