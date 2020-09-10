9480270c9581aad4469a6e3b7cd6576c67c4ff6d
Larissa <me@larissalittle.com>
2020-09-10 19:22:57 -0400

    PP2 Ex1 done

---
+++ pair_programming/exercise_1.sh
@@ -0,0 +1,25 @@
+#/!bin/bash
+
+read -r -p "What is the name of the file you want to commit? " FILE 
+git add $FILE
+git status
+read -r -p "Do you want to continue (Y/N)? " YN
+if [ "$YN" = "N" ]; then
+	exit 1
+elif [ "$YN" = "Y" ]; then
+	read -r -p "Type your commit message : " MSG
+else
+	echo "You must type Y or N"
+	exit 1
+fi
+
+git commit -m "$MSG"
+git status
+read -r -p "Do you want to continue? (Y/N)? " YN2
+if [ "$YN" = "N" ]; then
+	exit 1
+elif [ "$YN" = "Y" ]; then
+	git push
+else
+	echo "You mush type Y or N"
+	exit 1
